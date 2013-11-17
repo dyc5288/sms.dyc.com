3f1c5de672d8eb9a6c821648f10dad1ca849b0ba
duanyunchao <duanyunchao@115.com>
2013-11-17 10:27:18 +0800

    邮件平台

--- phpscript/install.php
+++ phpscript/install.php
@@ -14,28 +14,33 @@ $flag = hlp_common::get_cmd_flag();
 
 if (!empty($flag['help']))
 {
-    echo "/usr/sbin/gearmand --pid-file=/var/run/gearman/gearmand.pid --user=gearman --daemon --log-file=/var/log/gearman-job-server/gearman.log --listen=127.0.0.1" . PHP_EOL;
-    echo "/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -C 5 -u www-data -g www-data -f /usr/bin/php5-cgi -P /var/run/fastcgi-php.pid" . PHP_EOL;
-    echo "/usr/bin/memcached -m 64 -p 11211 -u memcache -l 127.0.0.1" . PHP_EOL;
-    echo "php install.php -test 1" . PHP_EOL;
+    //echo "/usr/sbin/gearmand --pid-file=/var/run/gearman/gearmand.pid --user=gearman --daemon --log-file=/var/log/gearman-job-server/gearman.log --listen=127.0.0.1" . PHP_EOL;
+    //echo "/usr/bin/spawn-fcgi -a 127.0.0.1 -p 9000 -C 5 -u www-data -g www-data -f /usr/bin/php5-cgi -P /var/run/fastcgi-php.pid" . PHP_EOL;
+    //echo "/usr/bin/memcached -m 64 -p 11211 -u memcache -l 127.0.0.1" . PHP_EOL;
+    echo "php install.php -dyc_clock 1" . PHP_EOL;
 }
 
-if (!empty($flag['test']))
+if (!empty($flag['dyc_clock']))
 {
     $number = 1;
     $sql    = "CREATE TABLE IF NOT EXISTS `%s` (
-        `tid` int(11) NOT NULL DEFAULT '0' COMMENT 'ID',
-        `test` varchar(50) NOT NULL DEFAULT '' COMMENT '内容',
-        PRIMARY KEY (`tid`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='测试'";
+        `cid` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+        `type_id` tinyint(4) NOT NULL DEFAULT '0' COMMENT  '类型ID',
+        `data` text NOT NULL DEFAULT '' COMMENT '数据根据具体业务不一样',
+        `ctime` int(10) DEFAULT NULL DEFAULT '0' COMMENT '添加时间',
+        `utime` int(10) DEFAULT NULL DEFAULT '0' COMMENT '修改时间',
+        `state` tinyint(4) DEFAULT NULL DEFAULT '0' COMMENT '0：关闭，1：开启，2：删除',
+        `remark` varchar(50) NOT NULL DEFAULT '' COMMENT '内容',
+        PRIMARY KEY (`cid`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='定时报警'";
 
-    if ($flag['test'] == 1)
+    if ($flag['dyc_clock'] == 1)
     {
-        hlp_tool::create_table('test', $sql, $number);
+        hlp_tool::create_table('dyc_clock', $sql, $number);
     }
-    else if ($flag['test'] == 2)
+    else if ($flag['dyc_clock'] == 2)
     {
-        hlp_tool::drop_table('test', $number);
+        hlp_tool::drop_table('dyc_clock', $number);
     }
 }
 
