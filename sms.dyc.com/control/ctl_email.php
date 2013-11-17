9538484ab8c9b1a6041d74edae1e93cf8f4910a5
duanyunchao <duanyunchao@115.com>
2013-11-17 15:01:12 +0800

    邮件平台

--- sms.dyc.com/control/ctl_email.php
+++ sms.dyc.com/control/ctl_email.php
@@ -33,6 +33,55 @@ class ctl_email extends ctl_parent
         lib_template::display('email_index.tpl');
     }
 
+    /**
+     * 白银
+     * 
+     * @return void
+     */
+    public function silver()
+    {
+        $return = array('state'   => false, 'message' => '');
+        $form     = get_params('form', 3);
+
+        if (!empty($form))
+        {
+            try
+            {
+                $data = array();
+                $data['hign'] = isset($form['hign']) ? $form['hign'] : 0;
+                $data['low']  = isset($form['low']) ? $form['low'] : 0;
+                $cid          = isset($form['cid']) ? $form['cid'] : 0;
+                $startup      = isset($form['startup']) ? $form['startup'] : 0;
+
+                if (empty($data['hign']) || empty($data['low']))
+                {
+                    T(10001);
+                }
+
+                $params = array();
+                $params['data']    = $data;
+                $params['remark']  = isset($form['remark']) ? $form['remark'] : '';
+                $params['state']   = $startup ? pub_mod_clock::STATE_STARTUP : pub_mod_clock::STATE_DEFAULT;
+                $params['type_id'] = pub_mod_clock::TYPE_SILVER;
+                $result            = pub_mod_clock::edit_clock($cid, $params);
+
+                if (empty($result))
+                {
+                    T(10000);
+                }
+
+                goto_url(URL . '/?ct=email');
+                $return['state'] = true;
+            }
+            catch (Exception $e)
+            {
+                $return['message'] = $e->getMessage();
+            }
+        }
+
+        lib_template::display('email_silver.tpl');
+    }
+
 }
 
 ?>
