f46952ebb936bfd211f8c2e77181d13e301b4dcd
duanyunchao <duanyunchao@115.com>
2013-11-17 11:01:53 +0800

    邮件平台

---
+++ model/pub_mod_clock.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * 定时报警 
+ * 
+ * @author duanyunchao
+ * @version $Id$
+ */
+class pub_mod_clock
+{
+    /* 状态 */
+    const STATE_DEFAULT = 0;
+    const STATE_STARTUP = 1;
+    const STATE_DELETE  = 2;
+
+    /**
+     * 获取一条
+     *
+     * @param int $cid
+     * @return array
+     */
+    public static function get_clock($cid)
+    {
+        if (empty($cid))
+        {
+            return false;
+        }
+
+        return dbc_clock::get_one($cid);
+    }
+
+    /**
+     * 添加
+     * 
+     * @return void
+     */
+    public static function edit_clock($cid, $params)
+    {
+        if (empty($params))
+        {
+            return false;
+        }
+
+        $clock_obj = dbc_clock::get_one($cid);
+        $column    = array('type_id', 'data', 'state', 'remark');
+        $param_array = array();
+
+        foreach ($column as $column_name)
+        {
+            if (isset($params[$column_name]))
+            {
+                switch ($column_name)
+                {
+                    case 'data':
+                        $params[$column_name] = urlencode(json_encode($params[$column_name]));
+                        break;
+                }
+
+                if (empty($clock_obj) || $clock_obj[$column_name] != $params[$column_name])
+                {
+                    $param_array[$column_name] = $params[$column_name];
+                }
+            }
+        }
+
+        if (empty($param_array))
+        {
+            return true;
+        }
+
+        if (!empty($clock_obj))
+        {
+            return dbc_clock::update($cid, $param_array);
+        }
+        else
+        {
+            return dbc_clock::insert($param_array);
+        }
+    }
+
+    /**
+     * 获取列表
+     * 
+     * @return void
+     */
+    public static function get_list($cond, $order, $start = 0, $limit = 10)
+    {
+        $start = intval($start);
+        $limit = intval($limit);
+        return dbc_clock::get_list($cond, $order, $start, $limit);
+    }
+
+    /**
+     * 获取个数
+     * 
+     * @return void
+     */
+    public static function get_count($cond)
+    {
+        return dbc_clock::get_count($cond);
+    }
+
+    /**
+     * 获取所有启动
+     *
+     * @return array 
+     */
+    public static function get_all_startup()
+    {
+        return dbc_user_address::get_all_startup();
+    }
+
+}
