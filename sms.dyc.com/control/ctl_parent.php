892fb3db3a902d2b29be09e1237a6590f1936b95
duanyunchao <duanyunchao@115.com>
2013-11-17 08:24:20 +0800

    邮件平台

---
+++ sms.dyc.com/control/ctl_parent.php
@@ -0,0 +1,24 @@
+<?php
+
+!defined('IN_INIT') && exit('Access Denied');
+
+/**
+ * 父类控制器
+ *
+ * @author duanyunchao
+ * @version $Id$
+ */
+class ctl_parent
+{
+    /**
+     * 初始化数据
+     * 
+     * @return void
+     */
+    protected function __construct()
+    {
+        ;
+    }
+}
+
+?>
