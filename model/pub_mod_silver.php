6f2916682e0be9f20b5f8ab82282baa3fd8ef8e0
duanyunchao <duanyunchao@115.com>
2013-11-20 21:32:57 +0800

    白银

--- model/pub_mod_silver.php
+++ model/pub_mod_silver.php
@@ -39,5 +39,61 @@ class pub_mod_silver
         return array('price' => $cur_price, 'time'  => $cur_time);
     }
 
+    /**
+     * 获取当前白银价格
+     * 
+     * @return int
+     */
+    public static function get_curr_price()
+    {
+        $port   = 9003;
+        $ip     = "183.62.250.10";
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        if ($socket < 0)
+        {
+            echo "socket_create() failed: reason: " . socket_strerror($socket) . "\n";
+        }
+        else
+        {
+            echo "CREATE　OK.\n";
+        }
+
+        echo "试图连接 '$ip' 端口 '$port'...\n";
+        $result = socket_connect($socket, $ip, $port);
+
+        if ($result < 0)
+        {
+            echo "socket_connect() failed.\nReason: ($result) " . socket_strerror($result) . "\n";
+        }
+        else
+        {
+            echo "CONNECT OK\n";
+        }
+
+        $in  = "ez\r\n";
+        $out = '';
+
+        if (!socket_write($socket, $in, strlen($in)))
+        {
+            echo "socket_write() failed: reason: " . socket_strerror($socket) . "\n";
+        }
+        else
+        {
+            echo "SEND OK！" . PHP_EOL;
+            echo "发送的内容为:{$in}" . PHP_EOL;
+        }
+
+        while ($out = socket_read($socket, 8192))
+        {
+            echo "接收服务器回传信息成功！\n";
+            echo "接受的内容为:", $out;
+        }
+
+        echo "关闭SOCKET...\n";
+        socket_close($socket);
+        echo "关闭OK\n";
+    }
+
 }
 
